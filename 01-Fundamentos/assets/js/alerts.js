cf27acf2b1a4d3f03fa23555fdd1c397b2ad0246
Nidia <nidiaglezcepeda2@gmail.com>
2024-10-30 21:00:23 -0600

    Primer version

---
+++ 01-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,7 @@
+//alert("Hola Chanitos");
+//let nombre = prompt("¿Cual es tu nombre?");
+//console.log(nombre);
+//console.log("*****" + nombre + "*****");//"" el valor que esta entre las comilas es un string vacio
+//const seleccion = confirm("Estas seguro de borrar esto?");
+//console.log(seleccion);
+console.log(global)
---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,19 @@
+alert('Hola desde app.js');
+//console.log("Hola mundo");
+
+ let a=10,
+  b=11,
+  c='Hola',
+  d='Spiderman',
+  f=a+b;
+
+  const saludo = c + d;
+
+ //console.log('%c Mis variables', 'color:blue; font-weight: bold');
+ //console.log({a},{b},{c},{d},{f});
+ //console.table({a,b,c,d,f}); 
+
+ c='hola de nuevo';
+
+// console.table({saludo,a,b,c,d,f});
+const outerWidth = 2900;
